e9530d6aca4ca4b85311a5817d6b283c78264a3a
twostars <i.am.twostars@gmail.com>
2013-07-09 19:12:39 +1000

    Allowed for caching of requests across the processes runtime.

--- Source/GitIoSharp/GitIo.cs
+++ Source/GitIoSharp/GitIo.cs
@@ -1,24 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GitIoSharp
 {
 	public class GitIo
 	{
 		private readonly GitIoService _gitIoService;
+        private readonly Dictionary<string, Uri> _urlCache;
 
 		public GitIo()
 		{
-			_gitIoService = new GitIoService();
+            _gitIoService = new GitIoService();
+            _urlCache = new Dictionary<string, Uri>();
 		}
+
+        private Uri AttemptLookup(string url)
+        {
+            var key = url;
+            if (_urlCache.ContainsKey(key))
+                return _urlCache[key];
+
+			var shortUri = _gitIoService.Execute(url);
+            _urlCache.Add(key, shortUri);
+            return shortUri;
+        }
 
 		public Uri Shorten(Uri url)
 		{
-			return _gitIoService.Execute(String.Format("url={0}", url));
+            return AttemptLookup(String.Format("url={0}", url));
 		}
 
 		public Uri Shorten(Uri url, string code)
 		{
-			return _gitIoService.Execute(String.Format("url={0}&code={1}", url, code));
+            return AttemptLookup(String.Format("url={0}&code={1}", url, code));
 		}
 	}
 }
